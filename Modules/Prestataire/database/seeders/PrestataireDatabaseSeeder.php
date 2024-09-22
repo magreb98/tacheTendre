469c8fd1851cf823b1381559107cbef0b36b7cca
magreb98 <magreb98>
2024-09-22 23:01:04 +0100

    initialisation du projet avec l'autentification

---
+++ Modules/Prestataire/database/seeders/PrestataireDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Prestataire\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PrestataireDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
